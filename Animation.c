f03e4118296744afefe7b5655d0f977d8901922c
Juliano Zatta <julianokzatta@gmail.com>
2016-07-11 15:11:58 -0300

    Animation OK, must implement values

--- Animation.c
+++ Animation.c
@@ -7,27 +7,26 @@
 #define MAXFRAME      16*(smoothDegree)
 
 int frame = 0;
-char smoothDegree = 2;
+char smoothDegree = 8;
 
 const float MovX[16][ANIM_MATRIX] = {
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Trunk                    0                     */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Neck                     1                     */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Head                     2                     */
-  {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Tail                     3                     */
-  {  0.0,  5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 35.0, 30.0, 25.0, 20.0, 15.0, 10.0,  5.0}, /* Left Back Leg Joint      4                     */
+  {  0.0, 10.0, 10.0, 10.0,  0.0, 10.0, 10.0, 10.0,  0.0, 10.0, 10.0, 10.0,  0.0, 10.0, 10.0, 10.0}, /* Tail                     3                     */
+  {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Left Back Leg Joint      4                     */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Left Back leg            5                     */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Lower Left Back Leg      6                     */
-  {  0.0,  2.0,  4.0,  6.0,  8.0, 10.0, 12.0, 14.0, 16.0, 14.0, 12.0, 10.0,  8.0,  6.0,  4.0,  2.0}, /* Right Back Leg Joint     7                     */
+  {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Right Back Leg Joint     7                     */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Right Back Leg           8                     */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Lower Right Back Leg     9                     */
-  {  0.0,  2.0,  4.0,  6.0,  8.0, 10.0, 12.0, 14.0, 16.0, 14.0, 12.0, 10.0,  8.0,  6.0,  4.0,  2.0}, /* Left Front Leg Joint     10                    */
+  {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Left Front Leg Joint     10                    */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Left Front Leg           11                    */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Lower Left Front Leg     12                    */
-  {  0.0,  5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 35.0, 30.0, 25.0, 20.0, 15.0, 10.0,  5.0}, /* Right Front Leg Joint    13                    */
+  {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Right Front Leg Joint    13                    */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}, /* Right Front Leg          14                    */
   {  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0}  /* Lower Right Front Leg    15                    */
 };
-
 void calculateMatrix(void) {
   // Prevent matrix overflow
   frame %= MAXFRAME;
@@ -35,7 +34,8 @@ void calculateMatrix(void) {
   for (int i = 0; i < 16; i++) {
     float averageAngle;
     int frameSmooth = frame/smoothDegree;
-    averageAngle = (MovX[i][frameSmooth] + MovX[i][frameSmooth+1]) / 2.0;
+    int frameSmoothPlusOne = ((frame/smoothDegree)+1) % ANIM_MATRIX;
+    averageAngle = (MovX[i][frameSmooth] * (1.0 - ((float)(frame%smoothDegree)/(float)smoothDegree))) + (MovX[i][frameSmoothPlusOne] * ((float)(frame%smoothDegree)/(float)smoothDegree));
     angles[i][0] = averageAngle;
     angles[i][1] = 0.0;
     angles[i][2] = 0.0;
